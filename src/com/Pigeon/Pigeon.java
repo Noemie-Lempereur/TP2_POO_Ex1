a671593bf1155f9654228b21592052b539a75bba
Noemie-Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-10-25 15:11:03 -0400

    Update Pigeon.java
    Ajout des attributs et methodes de bases de la classe pigeon

--- src/com/Pigeon/Pigeon.java
+++ src/com/Pigeon/Pigeon.java
@@ -3,5 +3,37 @@ package com.Pigeon;
 import java.lang.Thread;
 
 public class Pigeon {
+package com.Pigeon;
+
+import java.lang.Thread;
+
+public class Pigeon {
+    private int coordonnee;
+    private enum etatPigeon{Endormi, Reveille};
+    private Pigeon.etatPigeon etat;
+
+    public Pigeon(int coordonneeNew) {
+        this.etat = etatPigeon.Reveille;
+        this.coordonnee = coordonneeNew;
+    }
+
+    public int getCoordonnee(){
+        return coordonnee;
+    }
+
+    public etatPigeon getEtat(){
+        return etat;
+    }
+
+    public void setCoordonnee(int coordonneeNew){
+        coordonnee=coordonneeNew;
+    }
+
+    public void setEtat(etatPigeon etatNew){
+        etat=etatNew;
+    }
+
+
+}
 
 }
