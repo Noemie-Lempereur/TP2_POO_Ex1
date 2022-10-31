4225fcda20e097e34baf928ab75c034d97914855
Noémie Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-10-31 12:04:30 -0400

    Update pigeon
    Ajout des pigeons qui mangent la nourriture fraiche

--- src/com/Pigeon/Pigeon.java
+++ src/com/Pigeon/Pigeon.java
@@ -1,6 +1,8 @@
 import java.util.Calendar;
 import java.util.Date;
 
+import Nourriture.etatNourriture;
+
 public class Pigeon extends Thread {
     private int coordonnee;
 
@@ -31,16 +33,37 @@ public class Pigeon extends Thread {
         etat = etatNew;
     }
 
+    public void resetPlusFraiche() {
+        Main.plusFraiche.setCoordonnee(0);
+
+        Calendar calendrier = Calendar.getInstance();
+        calendrier.set(Calendar.YEAR, 2000);
+        calendrier.set(Calendar.MONTH, 1);
+        calendrier.set(Calendar.DAY_OF_MONTH, 1);
+        Date DatePlusFraiche = calendrier.getTime();
+
+        Nourriture NourritureFraiche = new Nourriture(0);
+        for (Nourriture n : Main.nourritures) {
+            if (n.getDate().getTime() - DatePlusFraiche.getTime() > 0) {
+                DatePlusFraiche = n.getDate();
+                NourritureFraiche = n;
+            }
+        }
+        NourritureFraiche.setEtatPlusFraiche();
+    }
+
     public void run() {
         try {
             while (!this.isInterrupted()) {
-                System.out.println("Hola " + this.getCoordonnee());
+                // System.out.println("Hola " + this.getCoordonnee());
                 if (Main.nourritures.size() > 0) {
                     this.setEtat(etatPigeon.Reveille);
                     Thread.sleep(500);
                     int coord = this.getCoordonnee();
                     if (coord == Main.plusFraiche.getCoordonneeN()) {
                         Main.nourritures.remove(Main.plusFraiche);
+                        resetPlusFraiche();
+                        Main.pigeons.remove(this);
                         this.interrupt();
                     }
                     if (coord < Main.plusFraiche.getCoordonneeN()) {
@@ -49,27 +72,18 @@ public class Pigeon extends Thread {
                         coord--;
                     }
                     this.setCoordonnee(coord);
+                    for (Nourriture n : Main.nourritures) {
+                        if (n.getCoordonneeN() == this.getCoordonnee()
+                                && n.getEtat() == Nourriture.etatNourriture.Fraiche) {
+                            Main.nourritures.remove(n);
+                            this.interrupt();
+                        }
+                    }
 
                     System.out.println("Hola " + this.getCoordonnee());
                     if (coord == Main.plusFraiche.getCoordonneeN()) {
                         Main.nourritures.remove(Main.plusFraiche);
-                        Main.plusFraiche.setCoordonnee(0);
-
-                        Calendar calendrier = Calendar.getInstance();
-                        calendrier.set(Calendar.YEAR, 2000);
-                        calendrier.set(Calendar.MONTH, 1);
-                        calendrier.set(Calendar.DAY_OF_MONTH, 1);
-                        Date DatePlusFraiche = calendrier.getTime();
-
-                        Nourriture NourritureFraiche = new Nourriture(0);
-                        for (Nourriture n : Main.nourritures) {
-                            if (n.getDate().getTime() - DatePlusFraiche.getTime() > 0) {
-                                DatePlusFraiche = n.getDate();
-                                NourritureFraiche = n;
-                            }
-                        }
-                        NourritureFraiche.setEtatPlusFraiche();
-
+                        resetPlusFraiche();
                         Main.pigeons.remove(this);
                         this.interrupt();
                     }
